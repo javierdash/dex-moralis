b831f5f0dd0b5cfea883d7775f54758bc5e3b915
javier.bru <javier@bru.finance>
2022-03-14 10:56:41 -0300

    last changes

--- dex.js
+++ dex.js
@@ -1,9 +1,10 @@
-//require('dotenv').config();
+//require('dotenv').config(); 
 
 // connect to Moralis server
 const serverUrl = "https://kjfkhnisfbja.usemoralis.com:2053/server"
 const appId = "7R1AtAtDI0yqorQVwB9Fjs6Dko0QJkp1Ye6vzJlJ"
 Moralis.start({ serverUrl, appId });
+
 
 Moralis
     .initPlugins()
@@ -13,12 +14,11 @@ const $tokenBalanceTBody = document.querySelector('.js-token-balances');
 const $selectedToken = document.querySelector('.js-from-token');
 const $amountInput = document.querySelector('.js-from-amount');
 
-/** Utilities */
-//NO ENCONTRÉ. Converting from Wei using custom Function
+// Utilities 
 const tokenValue = (value, decimals) => 
     (decimals? value / Math.pow(10, decimals) : value);
 
-/** Login-Logout and initialization */
+// Login-Logout and initialization 
 // add from here down
 async function login() {
     let user = Moralis.User.current();
@@ -29,7 +29,7 @@ async function login() {
     getStats();
   }
 
-  async function initSwapForm(event) {
+async function initSwapForm(event) {
       event.preventDefault();
       $selectedToken.innerText = event.target.dataset.symbol;
       $selectedToken.dataset.address = event.target.dataset.address;
@@ -40,21 +40,21 @@ async function login() {
       document.querySelector('js-submit').removeAttribute('disabled');
       document.querySelector('js-cancel').removeAttribute('disabled');
       document.querySelector('js-quote-container').innerHTML = '';
-      document.querySelector('.js-amount-error').innerText = '';       
+  //    document.querySelector('.js-amount-error').innerText = '';       
   }
 
-
 async function getStats() {
-    const balances = await Moralis.Web3API.account.getTokenBalances({chain: 'polygon'});
+    const balances = await Moralis.Web3API.account.getTokenBalances({chain: 'eth'});
     console.log(balances);
+
     $tokenBalanceTBody.innerHTML = balances.map( (token, index) => `
         <tr>
             <td>${index +1}</td>
             <td>${token.symbol}</td>
             <td>${tokenValue(token.balance, token.decimals)}</td>
-            <td>
-                <button
-                    class="js-swap"
+            <td>            
+            <button
+                    class="js-swap btn btn-success"
                     data-address="${token.token_address}"
                     data-symbol="${token.symbol}"
                     data-decimals="${token.decimals}"
@@ -67,21 +67,25 @@ async function getStats() {
     `).join('');
     
     for(let $btn of $tokenBalanceTBody.querySelectorAll('.js-swap')) {
-        $btn.addEventListener('click', swap);
+        $btn.addEventListener('click', initSwapForm);
     }
 
 }
-
-async function buyCrytpo() {
-    Moralis.Plugins.fiat.buy();
-}
-
 
 async function logOut() {
     await Moralis.User.logOut();
     console.log("logged out");
   }
 
+  document.getElementById("btn-login").addEventListener('click', login);
+  document.getElementById("btn-logout").addEventListener('click', logOut);
+
+/*
+
+async function buyCrytpo() {
+    Moralis.Plugins.fiat.buy();
+}
+ 
   document.querySelector("#btn-login").addEventListener('click', login);
   document
         .getElementById("btn-buy-crypto")
@@ -89,7 +93,9 @@ async function logOut() {
 
   document.getElementById("btn-logout").addEventListener('click', logOut);
 
-/** Quote/Swap */
+*/
+
+//Quote/Swap 
 async function formSubmitted(event) {
     event.preventDefault();
     const fromAmount = Number.parseFloat($amountInput.value);
@@ -102,34 +108,7 @@ async function formSubmitted(event) {
     } else {
         document.querySelector('.js-amount-error').innerText = '';       
     }
-
-    // Submission of the quote request
-    const fromDecimals = $selectedToken.dataset.decimals;
-    const fromTokenAddress = $selectedToken.dataset.address;
-
-    const [toTokenAddress, toDecimals] = document.querySelector('[name=to-token]').value.split('-');
-
-    try {
-        const quote = await Moralis.Plugins.oneInch.quote({
-            chain: 'polygon', // The blockchain you want to use (eth/bsc/polygon)
-            fromTokenAddress: fromTokenAddress, // The token you want to swap
-            toTokenAddress: toTokenAddress, // The token you want to receive
-            amount: Moralis.Units.Token(fromAmount, fromDecimals).toString(),
-          });
-
-          const toAmount = tokenValue(quote.toTokenAmount, toDecimals);
-          document.querySelector.apply('.js-quote-container').innerHTML = `
-            <p>${fromAmount} ${quote.fromToken.symbol} = ${toAmount} ${quote.toToken.symbol}</p>
-        `;
-
-    } catch(e) {
-        document.querySelector.apply('.js-quote-container').innerHTML = `
-            <p class="error">The conversion didnt succeed ERRORROROR</p>
-        `;
-    }
-
 }
-
 
 async function formCanceled(event) {
     event.preventDefault();
@@ -144,45 +123,83 @@ async function formCanceled(event) {
     document.querySelector('.js-amount-error').innerText = '';       
 }
 
-
 document.querySelector('.js-submit').addEventListener('click', formSubmitted);
 document.querySelector('.js-cancel').addEventListener('click', formCanceled);
 
 
 
 
-// CAMBIAR POR EL DE MORALIS QUE FUNCIONA! APIII
-/** To token dropdown preparation*/
+// To token dropdown preparation
 async function getTop10Tokens() {
     const response = await fetch('https://api.coinpaprika.com/v1/coins');
     const tokens = await response.json();
-
+//    console.log(tokens);
     return tokens
-            .filter(token => token.rank >=1 && token.rank <= 30)
+            .filter(token => token.rank >=1 && token.rank <= 20)
             .map(token => token.symbol);
-
 }
+
 async function getTickerData(tickerList) {
-    const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
-        chain: 'polygon',
-    }); 
+    //const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({ DOESN´T WORK 
+    //   chain: 'eth',
+    //});
+    const response = await fetch('https://api.1inch.exchange/v3.0/1/tokens'); //says "includes" is undefined on console
+    const tokens = await response.json();
     const tokenList = Object.values(tokens.tokens);
 
-    return tokenList.filter(token => tickerList.includes(token.symbol));
-}         
-
-function renderTokenDropdown(tokens) {
-    const options = tokens.map(token => `
-        <option value="${token.address}-${token.decimals}">
-        ${token.name}
-        </option>
-    `).join('');
-    document.querySelector('[name=to-token]').innerHTML = options;
+    const data = tokenList.filter(token => tickerList.includes(token.symbol));  
+    console.log("tickerData", data);
+    return data;
 }
 
-getTop10Tokens()
-    .then(getTickerData)
-    .then(renderTokenDropdown);
+
+
+//THIS DOESNT FIND MAP...
+function renderTokenDropdown(tokens) {
+  //  console.log(tokens)
+    const options = tokens.map(token => `
+        <option value="${token.address}-${token.decimals}">
+            ${token.name}
+        </option>
+        `).join('');
+       
+ //   document.querySelector('[name=from-token]').innerHTML = options;
+    document.querySelector('[name=to-token]').innerHTML = options;
+    document.querySelector('.js-submit').removeAttribute('disabled');
+}
 
 
 
+async function formSubmitted(event) {
+    event.preventDefault();
+    const fromToken = document.querySelector('[name=from-token]').value;
+    const toToken = document.querySelector('[name=to-token]').value;
+    const [fromDecimals, fromAddress] = fromToken.split('-'); 
+    const [toDecimals, toAddress] = toToken.split('-');
+    const fromUnit = 10 ** fromDecimals;
+    const decimalRatio = 10 ** (fromDecimals - toDecimals);
+
+    const url = `https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=${fromAddress}&toTokenAddress=${toAddress}&amount=${fromUnit}`;
+    
+    try {
+    //console.log(url);
+    const response = await fetch(url);
+    const quote = await response.json();
+    const exchange_rate = Number(quote.toTokenAmount) / Number(quote.fromTokenAmount) * decimalRatio;
+    document.querySelector('.js-quote-container').innerHTML = `
+        <p>1 ${quote.fromToken.symbol} = ${exchange_rate} ${quote.toToken.symbol} </p>
+        <p>Gas fee: ${quote.estimatedGas}</p>
+    `;
+  } catch(e) {
+    document.querySelector('.js-quote-container').innerHTML = `you cant converted to the same coin!`
+  }
+}
+
+document
+    .querySelector('.js-submit')
+    .addEventListener('click', formSubmitted);
+
+  getTop10Tokens()             
+             .then(getTickerData) 
+//             .then(console.log)
+             .then(renderTokenDropdown);
